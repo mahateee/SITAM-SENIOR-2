2593aace510e09a0b5b31d49f52b3e7a801207cc
thisdana <70285707+thisdana@users.noreply.github.com>
2023-09-16 20:54:47 +0300

    Added a Show Asset page (generated by useblackbox.ai )

--- src/pages/AddAsset.js
+++ src/pages/AddAsset.js
@@ -351,7 +351,7 @@ export default function AddAsset() {
                   className="block text-gray-700 font-bold mb-2"
                   htmlFor="os"
                 >
-                  Os:
+                  Operating System:
                 </label>
                 <input
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
