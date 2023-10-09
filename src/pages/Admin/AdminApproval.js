6a6e6c631ea42a3e617f390132fef05e7e8cbde3
thisdana <70285707+thisdana@users.noreply.github.com>
2023-10-09 22:35:20 +0300

    UI update

--- src/pages/Admin/AdminApproval.js
+++ src/pages/Admin/AdminApproval.js
@@ -42,7 +42,7 @@ const AdminApprovalTable = ({ requests, onApprove, onReject }) => (
                   </th>
                 </tr>
               </thead>
-              <tbody class="bg-white ">
+              <tbody class="bg-white text-center ">
                 {requests.length > 0 ? (
                   requests.map((req, id) => (
                     <tr key={id} className="border-b">
@@ -52,7 +52,10 @@ const AdminApprovalTable = ({ requests, onApprove, onReject }) => (
                       >
                         {req.name + " " + req.lastname}
                       </th>
+<td className="p-4 text-sm font-normal   text-gray-900 whitespace-nowrap">
+$$$$
 
+</td>
                       <td className="p-4 text-sm font-normal text-gray-900 whitespace-nowrap">
                         <button
                           className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none"
