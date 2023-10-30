448aa86de08fdd71f89e90056bf91ca836e54bc3
Danah <danawattar@gmail.com>
2023-10-30 14:36:14 +0300

    1.Redesign return and maintanence buttons

--- src/component/CurrentAssets.js
+++ src/component/CurrentAssets.js
@@ -161,12 +161,12 @@ const AssetsTable = () => {
                         <button
                           onClick={() => handleReturn(asset.id, id)}
                           type="button"
-                          className="text-white bg-indigo-800 hover:bg-indigo-600 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 ml-6 mb-2 focus:outline-none">
+                          className="text-white bg-teal-800 hover:bg-teal-700 focus:ring-4 focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 ml-6 mb-2 focus:outline-none">
                           Return
                         </button>
                         <Link
                           to={`/Request/Maintenance/${asset.id}`}
-                          className="text-white bg-indigo-500 hover:bg-indigo-600 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 ml-6 mb-2 focus:outline-none">
+                          className="text-white bg-teal-500 hover:bg-teal-600 focus:ring-4 focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 ml-6 mb-2 focus:outline-none">
                             Maintenance</Link>
                       </td>
                     </tr>
